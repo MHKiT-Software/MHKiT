4927e6b83e5d1797fcb656533426c05a60b25318
akeeste <akeeste@sandia.gov>
2024-11-26 12:15:06 -0600

    fix minor typo in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ autosectionlabel_prefix_document = True
 #    
 
 # -- Path setup --------------------------------------------------------------
-# If extensions (or modules to document with autodoc) are in another dire	ctory,
+# If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
